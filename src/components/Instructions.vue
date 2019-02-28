326ff2ff76ccc454c99ac9df7909da61aef1e5af
Evgeny Belkin <sunafajro@gmail.com>
2019-02-28 23:44:50 +0300

    инструкция упрощена

--- src/components/Instructions.vue
+++ src/components/Instructions.vue
@@ -1,67 +1,27 @@
 <template>
   <div>
-    <div class="row">
+    <div class="row" style="margin-bottom: 1rem">
       <div class="col-12">
-        <c-intro-1/>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-6">
-        <c-intro-2/>
-      </div>
-      <div class="col-6">
-        <c-intro-3/>
-      </div>
-    </div>
-    <div class="row">
-      <div :class="examType === 'ege' ? 'col-6' : 'col-12'">
-        <c-intro-4/>
-      </div>
-      <div class="col-6" v-if="examType === 'ege'">
-        <c-intro-5/>
+        <div class="card">
+          <div class="card-body">
+            Сейчас вы будете выполнять устную часть ГИА-11. Ваши ответы
+            записываются. На экране вы видите время на подготовку (желтый
+            таймер). При выполнении задания вы также имеет ограничение по
+            времени (красный таймер).
+          </div>
+        </div>
       </div>
     </div>
     <div class="row">
       <div class="col-12 text-center">
-        <router-link class="btn btn-success" to="/exams">Все понятно</router-link>
+        <router-link class="btn btn-success" to="/exams">
+          Все понятно
+        </router-link>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
-import I1 from "./Instructions/I1.vue";
-import I2 from "./Instructions/I2.vue";
-import I3 from "./Instructions/I3.vue";
-import I4 from "./Instructions/I4.vue";
-import I5 from "./Instructions/I5.vue";
-
-export default {
-  components: {
-    "c-intro-1": I1,
-    "c-intro-2": I2,
-    "c-intro-3": I3,
-    "c-intro-4": I4,
-    "c-intro-5": I5
-  },
-  computed: {
-    ...mapState(["examType"])
-  }
-};
+export default {};
 </script>
-
-<style>
-.instructions-text-title {
-  font-size: 24px;
-  margin-bottom: 0.5rem;
-}
-.instructions-text-description {
-  font-size: 20px;
-  margin-bottom: 0.5rem;
-}
-.instructions-text-time-params {
-  font-size: 18px;
-  margin-bottom: 0.5rem;
-}
-</style>
