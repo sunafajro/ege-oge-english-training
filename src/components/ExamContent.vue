1ffe07b501684c1e6cd44be270fd9ea5fd5fb12f
Evgeny Belkin <sunafajro@gmail.com>
2019-03-03 22:31:00 +0300

    доработки по 2му заданию теста

--- src/components/ExamContent.vue
+++ src/components/ExamContent.vue
@@ -13,6 +13,9 @@
       />
     </div>
     <p>{{ task.description }}</p>
+    <p v-if="selectedQuestion !== null">
+      {{ time | timeFormat }}
+    </p>
     <ul v-if="filteredQuestions.length">
       <li
         :key="'question-' + index"
@@ -29,6 +32,8 @@
 
 <script>
 import { mapState } from "vuex";
+import { timeFormat } from "../utils";
+
 export default {
   computed: {
     ...mapState(["examType"]),
@@ -38,22 +43,64 @@ export default {
         : this.task.images;
     },
     filteredQuestions() {
-      if (this.task.sequentialQuestions) {
-        return this.task.questions;
+      if (this.task.sequentialQuestions && this.selectedQuestion !== null) {
+        return this.task.questions.slice(
+          this.selectedQuestion,
+          this.selectedQuestion + 1
+        );
       } else {
         return this.task.questions;
       }
     }
   },
+  created() {
+    if (this.task.sequentialQuestions) {
+      this.start(this.task.prepareTime);
+    }
+  },
   data() {
     return {
-      selectedImage: null
+      selectedQuestion: null,
+      selectedImage: null,
+      time: 0,
+      timer: null
     };
+  },
+  filters: {
+    timeFormat(seconds) {
+      return timeFormat(seconds);
+    }
   },
   methods: {
     selectImage(name) {
       if (this.task.selectableImages) {
         this.selectedImage = name;
+      }
+    },
+    start(s) {
+      if (s) {
+        this.time = s;
+      }
+      if (!this.timer) {
+        this.timer = setInterval(() => {
+          if (this.time > 0) {
+            this.time--;
+          } else {
+            this.stop();
+          }
+        }, 1000);
+      }
+    },
+    stop() {
+      clearInterval(this.timer);
+      this.time = 0;
+      this.timer = null;
+      if (this.selectedQuestion === null) {
+        this.selectedQuestion = 0;
+        this.start(this.task.questionsInterval);
+      } else if (this.selectedQuestion < this.task.questions.length - 1) {
+        this.selectedQuestion += 1;
+        this.start(this.task.questionsInterval);
       }
     }
   },
